328a538cddc5477bb0fc2e0f44017da825f06a60
Johan Lundberg <lundberg@sunet.se>
2019-10-15 17:24:06 +0200

    More debug logging for missing HTTP_EPPN

--- rpz_lookup/util.py
+++ rpz_lookup/util.py
@@ -12,6 +12,9 @@ def get_ipaddr_or_eppn():
     current_app.logger.debug('REQUEST ENVIRONMENT:')
     current_app.logger.debug(request.environ)
     identifier = request.environ.pop('HTTP_EPPN', None)
+    current_app.logger.debug(f'Identifier from request environment: {identifier}')
     if not identifier:
+        current_app.logger.warning('HTTP_EPPN is missing from request environment')
         identifier = get_ipaddr()
+        current_app.logger.debug(f'Identifier from get_idaddr: {identifier}')
     return identifier
